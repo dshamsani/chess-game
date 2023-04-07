e2a89f4dcff909b15e2951f7fcd8a75cae737dd2
David Shamsani <shamsani@list.ru>
2023-04-07 14:22:55 +0200

    feat: assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    colors: {
-      background: "#333333",
-    },
+    colors: {},
   },
   plugins: [],
 };
